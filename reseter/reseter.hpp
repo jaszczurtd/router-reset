652aa13cd23e30b6aecced071d6fa6ea9a85f468
Marcin Kielesiński <jaszczurtd@tlen.pl>
2025-09-19 16:25:47 +0200

    Base functionality done, pinging two adresses (1.1.1.1 / 8.8.8.8), measuring % of loss ping packets

--- reseter/reseter.hpp
+++ reseter/reseter.hpp
@@ -6,11 +6,30 @@
 #include <tools.h>
 #include <Credentials.h>
 
+#define ROUTER_RELAY_PIN 0
+
+#define DEVICE_DOMAIN "RouterWatchdog"
+
+#define MAX_DEAD_TIME 7000
+
+#define PING_ONE "1.1.1.1"
+#define PING_TWO "8.8.8.8"
+
 #define ON HIGH
 #define OFF LOW
 
 enum STATE {
   WIFI_NOT_CONNECTED = 0, WIFI_CONNECTING, WIFI_CONNECTED, WIFI_RESET_START, WIFI_RESET_END
 };
+const char *printableStates[] = {
+  "WIFI_NOT_CONNECTED", "WIFI_CONNECTING", "WIFI_CONNECTED", "WIFI_RESET_START", "WIFI_RESET_END"
+};
 
-#define WIFI_MAX_CONNECTION_MINUTES 5
+#define PING_TIMEOUT 1000
+
+#define MAX_PINGS 100
+#define MAX_PERCENT_OF_LOST_PINGS 75
+
+#define WIFI_MAX_CONNECTION_MINUTES 6
+#define WIFI_MAX_RESET_TIME_SECONDS 10
+
--- reseter/reseter.ino
+++ reseter/reseter.ino
@@ -3,28 +3,33 @@
 
 static int currentState = WIFI_NOT_CONNECTED;
 static int wifiConnectionProcessSeconds = 0;
-static String srv;
+static String srv1, srv2;
 
-IPAddress pingTarget(8, 8, 8, 8);
-WiFiClient client;
+static WiFiClient client;
+static IPAddress ping1Target, ping2Target;
+
+static int ping1Times[MAX_PINGS];
+static int ping1cnt = 0;
+static int ping2Times[MAX_PINGS];
+static int ping2cnt = 0;
 
 void routerEnabled(int state) {
-  digitalWrite(0, state); 
+  digitalWrite(ROUTER_RELAY_PIN, state); 
 }
 
 void startWifiConnection() {
-  deb("Laczenie z WiFi: %s", WIFI_SSID);
+  deb("trying to connect to WiFi: %s", WIFI_SSID);
 
   WiFi.disconnect(true);     
   WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.beginNoBlock(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void stopWifiConnection() {
-  deb("Rozlaczanie...");
+  deb("performing disconnection...");
 
   WiFi.disconnect(true);     
-  WiFi.end();
+  WiFi.mode(WIFI_OFF);
 }
 
 void setup() {
@@ -34,47 +39,73 @@ void setup() {
   pinMode(0, OUTPUT); //RP2040 PicoW pin 0
   digitalWrite(LED_BUILTIN, LOW);  
 
-  WiFi.setHostname("RouterWatchdog");
-  srv = pingTarget.toString();
+  WiFi.setHostname(DEVICE_DOMAIN);
+
+  //adresses defined in reseter.hpp
+  ping1Target.fromString(PING_ONE);
+  srv1 = ping1Target.toString();
+
+  ping2Target.fromString(PING_TWO);
+  srv2 = ping2Target.toString();
+
+  watchdog_enable(MAX_DEAD_TIME, false);  
 }
 
 static unsigned long t0;
 
-void loop() {
+void setState(int state) {
+  deb("set state from:%s to %s", printableStates[currentState], printableStates[state]);
+  currentState = state;
+}
 
-  unsigned long dt;
+void loop() {
+  watchdog_update();
 
   switch(currentState) {
     case WIFI_NOT_CONNECTED: {
-      startWifiConnection();
       routerEnabled(ON);
 
-      currentState = WIFI_CONNECTING;
+      setState(WIFI_CONNECTING);
       wifiConnectionProcessSeconds = 0;
+
+      ping1cnt = ping2cnt = 0;
+      memset(ping1Times, 0, sizeof(ping1Times));
+      memset(ping2Times, 0, sizeof(ping2Times));
+
+      startWifiConnection();
       t0 = millis();
     }
     break;
 
     case WIFI_CONNECTING: {
+      
       if(WiFi.status() == WL_CONNECTED) {
 
-        deb("Polaczono z wifi, adres IP: %s", WiFi.localIP().toString().c_str());
+        deb("connected to WiFi, IP: %s", WiFi.localIP().toString().c_str());
 
         digitalWrite(LED_BUILTIN, HIGH);  
+
+        WiFi.setTimeout(PING_TIMEOUT);
+
         t0 = millis();
-        currentState = WIFI_CONNECTED;
+        setState(WIFI_CONNECTED);
 
       } else {
         if(millis() - t0 > SECOND) {
           t0 = millis();
           digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-          deb("connection seconds: %ld", wifiConnectionProcessSeconds);
+          deb("connecting for %ld seconds...", wifiConnectionProcessSeconds);
 
-          if(++wifiConnectionProcessSeconds > (WIFI_MAX_CONNECTION_MINUTES * 60)) {
+          if(wifiConnectionProcessSeconds > 0 && 
+              wifiConnectionProcessSeconds % SECONDS_IN_MINUTE == 0) {
+            deb("restarting WiFi");
             stopWifiConnection();
-            routerEnabled(OFF);
-            currentState = WIFI_RESET_START;
+            startWifiConnection();
+          }
+
+          if(++wifiConnectionProcessSeconds > (WIFI_MAX_CONNECTION_MINUTES * SECONDS_IN_MINUTE)) {
+            setState(WIFI_RESET_START);
           }
         }
       }
@@ -82,28 +113,84 @@ void loop() {
     break;
 
     case WIFI_RESET_START: {
-
+      stopWifiConnection();
+      routerEnabled(OFF);
+      t0 = millis();
+      setState(WIFI_RESET_END);
     }
     break;
 
     case WIFI_RESET_END: {
-
+      if(millis() - t0 > SECOND * WIFI_MAX_RESET_TIME_SECONDS) {
+        setState(WIFI_NOT_CONNECTED);
+      }
     }
     break;
 
     case WIFI_CONNECTED: {
       if(millis() - t0 > SECOND) {
-        t0 = millis();
+        unsigned long t_ping;
+        int res1 = -1, res2 = -1;    
+        unsigned long dt1 = 0, dt2 = 0;
+        
+        t_ping = millis();
+        res1 = WiFi.ping(ping1Target); 
+        dt1 = millis() - t_ping;
+        watchdog_update();
 
-        unsigned long t_ping = millis();
-        int res = WiFi.ping(pingTarget); 
-        dt = millis() - t_ping;
-        if (res >= 0) {
-          deb("Ping do %s : %ld ms", srv.c_str(), dt);
+        t_ping = millis();
+        res2 = WiFi.ping(ping2Target); 
+        dt2 = millis() - t_ping;
+        watchdog_update();
+
+        if (res1 >= 0 || res2 >= 0) {
+          deb("Ping do %s / %s : %ld / %ld ms", srv1.c_str(), srv2.c_str(), dt1, dt2);
         } else {
-          deb("No response from %s (timeout)", srv.c_str());
-          //TODO:
+
+          if(res1 < 0) {
+            deb("No response from %s (timeout)", srv1.c_str());
+          }
+          if(res2 < 0) {
+            deb("No response from %s (timeout)", srv2.c_str());
+          }
+
         }  
+
+        ping1Times[ping1cnt] = res1 < 0 ? -1 : dt1;
+        ping1cnt++; if(ping1cnt >= MAX_PINGS) {
+          ping1cnt = 0;
+        }
+
+        ping2Times[ping2cnt] = res2 < 0 ? -1 : dt2;
+        ping2cnt++; if(ping2cnt >= MAX_PINGS) {
+          ping2cnt = 0;
+        }
+
+        int percentLost1 = 0;
+        int percentLost2 = 0;
+        int howManyLost1 = 0;
+        int howManyLost2 = 0;
+
+        for(int a = 0; a < MAX_PINGS; a++) {
+          if(ping1Times[a] < 0) {
+            howManyLost1++;
+          }
+          if(ping2Times[a] < 0) {
+            howManyLost2++;
+          }
+        }
+        percentLost1 = percentFrom(howManyLost1, MAX_PINGS);
+        percentLost2 = percentFrom(howManyLost2, MAX_PINGS);
+
+        deb("percent of lost packet for %s is:%d%%, for %s is:%d%%", 
+          srv1.c_str(), percentLost1, srv2.c_str(), percentLost2);
+
+        if(percentLost1 > MAX_PERCENT_OF_LOST_PINGS && 
+            percentLost2 > MAX_PERCENT_OF_LOST_PINGS) {
+            setState(WIFI_RESET_START);
+        }
+
+        t0 = millis();
       }
     }
     break;
